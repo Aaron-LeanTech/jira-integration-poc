90b0f23ed84b724e40d336425b345fc4d0db1c90
Aaron Delgado Ripoll <aaron@leantech.me>
2022-11-24 11:46:23 +0000

    init spring project

---
+++ src/main/java/com/example/jiraintegrationpoc/JiraIntegrationPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.jiraintegrationpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraIntegrationPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraIntegrationPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jiraintegrationpoc/JiraIntegrationPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jiraintegrationpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraIntegrationPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
